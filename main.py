66248ad25342daf1a456e39d52d1532cf52b2883
saurin16 <61411800+saurin16@users.noreply.github.com>
2021-09-12 18:31:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,70 @@
+import speech_recognition as sr
+import pyttsx3
+
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'jarvis' in command:
+                command = command.replace('jarvis', '')
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_jarvis():
+    command = take_command()
+
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'who the heck is' in command:
+        person = command.replace('who the heck is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+
+    elif 'date' in command:
+        talk('sorry, I have a headache')
+    elif 'are you single' in command:
+        talk('I am in a relationship with wifi')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+        print(pyjokes.get_joke())
+    elif 'who is your boss' in command:
+        talk('Saurin is my boss')
+    elif 'how are you' in command:
+        talk('I am great How are you?')
+
+    elif 'stop' in command:
+        talk('bye!!! have a great day!')
+    else:
+        talk('Please say the command again.')
+
+
+while True:
+    run_jarvis()
